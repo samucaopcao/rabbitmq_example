e6e6455d933c0c386bcb578b4a0b11bd30916a9f
Samuel M dos Santos <samuelmacedo2011@gmail.com>
2023-07-06 12:37:19 -0300

    Project creation

---
+++ rabbitmq_example/rabbitmq_example/src/main/java/br/com/study/rabbitmq_example/RabbitmqExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.study.rabbitmq_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqExampleApplication.class, args);
+	}
+
+}
---
+++ rabbitmq_example/rabbitmq_example/src/test/java/br/com/study/rabbitmq_example/RabbitmqExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.study.rabbitmq_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
